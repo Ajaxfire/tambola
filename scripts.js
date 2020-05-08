25d1d0cffebbf93a1d7f4d256a035edc7eca37d4
Ajaxfire <40214600+Ajaxfire@users.noreply.github.com>
2020-05-08 10:44:20 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ var keywords= [
   "","Two little ducks","","Two dozen","Silver Jubilee","Republic Day","Duck and Luck","","","",
   "","Teeth in mouth","All the 3s","","","","","","","",
   "","Quit India","","Saare chor","Halfway there","","Independence","Four dozen","","Golden Jubilee",
-  "Shagun","Pack of cards","","Five and four","","","","Retirement","","",
+  "Shagun","Pack of cards","","Five and four","All the fives","","","Retirement","","",
   "","","","","","All sixes","","","Ulta Pulta","Blind Lucky",
   "Lucky bachelor","Luck and duck","","","Diamond Jubilee","","","","","",
   "","","","","","","","Two Fat Majors","Almost on top","Top of the house"
@@ -42,23 +42,40 @@ returnRandomNumber=()=>{
   return -1;
 }
 
+toggleText=()=>{
+  var rootBtn = document.getElementsByClassName("btn")[0];
+  if(rootBtn.textContent.includes('Start')){
+    rootBtn.textContent = "⏸ Pause";
+  }
+  else{
+    rootBtn.textContent = "▶️ Start";
+  }
+}
+
+//For manual gameplay, call this function directly - btn onclick
 generateNumber=()=>{
   var nextNumber = returnRandomNumber();
   if(nextNumber<=0){
     //there has been a problem. or the game is over.
+    return;
   } else
   {
+    //toggleText(); //Enable when timer is available
     var rootDiv=document.getElementById("logTable");
     var newElement=document.createElement("div");
-    var announce = document.getElementById("announce");
+    var msg = document.getElementById("msg");
+    var num = document.getElementById("num");
     var currentNumber=document.createTextNode(nextNumber);
-    // useless delay end
-    // useless delay start
+    //Update Side Log
     newElement.appendChild(currentNumber);
     rootDiv.appendChild(newElement);
+    //Update Table
     var updateElement = document.getElementById(nextNumber);
     updateElement.classList.add("used");
     updateElement.classList.remove("notUsed");
-    announce.textContent = keywords[nextNumber]+" : "+nextNumber;
+    //update announcement
+    msg.textContent = keywords[nextNumber]!=""?keywords[nextNumber]+" : ":"";
+    num.textContent = nextNumber;
+    num.style.visibility="visible";
   }
 }
